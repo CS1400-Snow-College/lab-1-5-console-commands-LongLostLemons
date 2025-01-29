e3906c76c3a8e068daa10940e51a6052e04814c4
Brett <favrelous.56@gmail.com>
2025-01-29 13:35:49 -0700

    this hopefully works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,169 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.SetCursorPosition(0,1); //upper left corner
+Console.Write("                                    ");
+Console.SetCursorPosition(0,2);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,3);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,4);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,5);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,6);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,7);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,8);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,9);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,10);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,11);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,12);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,13);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,14);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,15);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,16);
+Console.Write("                                    ");
+Console.SetCursorPosition(0,17);
+Console.Write("                                    ");
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.SetCursorPosition(8,2); //hopefully under the first blue bar. 
+Console.Write("               ");
+Console.SetCursorPosition(6,3); //base of the hat on his head, I hope. 
+Console.Write("                        ");
+Console.SetCursorPosition(10,9); //Start of overalls. Maybe.
+Console.Write("  ");
+Console.SetCursorPosition(10,10);
+Console.Write("  ");
+Console.SetCursorPosition(19,10);
+Console.Write("  ");
+Console.SetCursorPosition(10,11);
+Console.Write("  ");
+Console.SetCursorPosition(19,11);
+Console.Write("  ");
+Console.SetCursorPosition(10,12);
+Console.Write("           ");
+Console.SetCursorPosition(8,13);
+Console.Write("  ");
+Console.SetCursorPosition(12,13);
+Console.Write("       ");
+Console.SetCursorPosition(21,13);
+Console.Write("  ");
+Console.SetCursorPosition(4,14);
+Console.Write("                     ");
+Console.SetCursorPosition(6,15);
+Console.Write("      ");
+Console.SetCursorPosition(19,15);
+Console.Write("      ");
+
+Console.BackgroundColor = ConsoleColor.Black; 
+Console.SetCursorPosition(6,4); //start of hair, a prayer is needed.
+Console.Write("       ");
+Console.SetCursorPosition(19,4);
+Console.Write("  ");
+Console.SetCursorPosition(4,5);
+Console.Write("  ");
+Console.SetCursorPosition(8,5);
+Console.Write("  ");
+Console.SetCursorPosition(19,5); // 19 is making me mad he is slightly stretch rez but I don't care 
+Console.Write("  ");
+Console.SetCursorPosition(4,6);
+Console.Write("  ");
+Console.SetCursorPosition(8,6);
+Console.Write("    ");
+Console.SetCursorPosition(21,6);
+Console.Write("  ");
+Console.SetCursorPosition(4,7);
+Console.Write("    ");
+Console.SetCursorPosition(19,7);
+Console.Write("         ");
+
+
+Console.BackgroundColor = ConsoleColor.Gray; // I was going to do yellow but then he would be the same color as his buttons.
+Console.SetCursorPosition(21,4);
+Console.Write("  ");
+Console.SetCursorPosition(12,4);
+Console.Write("       ");
+Console.SetCursorPosition(6,5);
+Console.Write("  ");
+Console.SetCursorPosition(10,5);
+Console.Write("         ");
+Console.SetCursorPosition(21,5);
+Console.Write("       ");
+Console.SetCursorPosition(6,6);
+Console.Write("  ");
+Console.SetCursorPosition(12,6);
+Console.Write("         ");
+Console.SetCursorPosition(23,6);
+Console.Write("       ");
+Console.SetCursorPosition(8,7);
+Console.Write("           ");
+Console.SetCursorPosition(8,8);
+Console.Write("                 ");
+Console.SetCursorPosition(2,13);
+Console.Write("      ");
+Console.SetCursorPosition(24,12);
+Console.Write("      ");
+Console.SetCursorPosition(2,12);
+Console.Write("      ");
+Console.SetCursorPosition(2,13);
+Console.Write("      ");
+Console.SetCursorPosition(23,13);
+Console.Write("       ");
+Console.SetCursorPosition(2,13);
+Console.Write("      ");
+Console.SetCursorPosition(2,14);
+Console.Write("    ");
+Console.SetCursorPosition(25,14);
+Console.Write("     ");
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.SetCursorPosition(6,9);
+Console.Write("    ");
+Console.SetCursorPosition(12,9);
+Console.Write("         ");
+Console.SetCursorPosition(4,10);
+Console.Write("      ");
+Console.SetCursorPosition(12,10);
+Console.Write("       ");
+Console.SetCursorPosition(21,10);
+Console.Write("       ");
+Console.SetCursorPosition(2,11);
+Console.Write("        ");
+Console.SetCursorPosition(12,11);
+Console.Write("       ");
+Console.SetCursorPosition(21,11);
+Console.Write("         ");
+Console.SetCursorPosition(6,12);
+Console.Write("    ");
+Console.SetCursorPosition(21,12);
+Console.Write("    ");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.SetCursorPosition(10,13);
+Console.Write("  ");
+Console.SetCursorPosition(19,13);
+Console.Write("  ");
+
+Console.BackgroundColor = ConsoleColor.DarkYellow; //There was no color closer to brown so he has drip now
+Console.SetCursorPosition(4,16);
+Console.Write("      ");
+Console.SetCursorPosition(19,16);
+Console.Write("      ");
+Console.SetCursorPosition(2,17);
+Console.Write("        ");
+Console.SetCursorPosition(19,17);
+Console.Write("        ");
+
+Console.BackgroundColor = ConsoleColor.Black; //The command line was eye burning yellow. 
